797145d8741c29fc4f37272d4b489acf51f451d8
Fattah Abdul Malik <fattahabdulm@gmail.com>
2026-01-10 19:30:43 +0700

    add console log

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const myAge = 27;
 
 console.log(myAge);
+
+console.log(2 == 2);
+
+console.log(true && false); // false
+
+console.log(2 > 1 || 3 < 5); //true
+
+console.log(false || true); //true
+
+console.log(!true); //false
+
+console.log(!(2 > 5)); //true
+
+const a = 20;
+const b = 30;
+
+console.log(a < b && b > 25); //true
+
+console.log(a > b || a === 10); //true
+
+const result = 5 > 4; // assign the result of the comparison
+
+console.log(result); // true
+
+console.log("Z" > "A"); // true
+
+console.log("Bee" > "Be"); //true
